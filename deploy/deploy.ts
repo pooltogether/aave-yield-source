c22f6fbb33b6d338b4572002a11079e01c6eeecd
Pierrick Turelier <pturelier@meilleursagents.com>
2021-03-10 23:56:41 +0100

    chore(hardhat): add deploy script

---
+++ deploy/deploy.ts
@@ -0,0 +1,102 @@
+import chalk from 'chalk';
+
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction, DeployResult } from 'hardhat-deploy/types';
+
+const displayLogs = !process.env.HIDE_DEPLOY_LOG;
+
+function dim(logMessage: string) {
+  if (displayLogs) {
+    console.log(chalk.dim(logMessage));
+  }
+}
+
+function cyan(logMessage: string) {
+  if (displayLogs) {
+    console.log(chalk.cyan(logMessage));
+  }
+}
+
+function yellow(logMessage: string) {
+  if (displayLogs) {
+    console.log(chalk.yellow(logMessage));
+  }
+}
+
+function green(logMessage: string) {
+  if (displayLogs) {
+    console.log(chalk.green(logMessage));
+  }
+}
+
+function displayResult(name: string, result: DeployResult) {
+  if (!result.newlyDeployed) {
+    yellow(`Re-used existing ${name} at ${result.address}`);
+  } else {
+    green(`${name} deployed at ${result.address}`);
+  }
+}
+
+const chainName = (chainId: number) => {
+  switch (chainId) {
+    case 1:
+      return 'Mainnet';
+    case 3:
+      return 'Ropsten';
+    case 4:
+      return 'Rinkeby';
+    case 5:
+      return 'Goerli';
+    case 42:
+      return 'Kovan';
+    case 77:
+      return 'POA Sokol';
+    case 99:
+      return 'POA';
+    case 100:
+      return 'xDai';
+    case 137:
+      return 'Matic';
+    case 31337:
+      return 'HardhatEVM';
+    case 80001:
+      return 'Matic (Mumbai)';
+    default:
+      return 'Unknown';
+  }
+};
+
+const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments, getChainId, ethers } = hre;
+  const { deploy } = deployments;
+
+  let { deployer, admin } = await getNamedAccounts();
+
+  const chainId = parseInt(await getChainId());
+
+  // 31337 is unit testing, 1337 is for coverage
+  const isTestEnvironment = chainId === 31337 || chainId === 1337;
+
+  const signer = ethers.provider.getSigner(deployer);
+
+  dim('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  dim('PoolTogether Aave Yield Source - Deploy Script');
+  dim('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+
+  dim(`network: ${chainName(chainId)} (${isTestEnvironment ? 'local' : 'remote'})`);
+  dim(`deployer: ${deployer}`);
+
+  if (!admin) {
+    admin = signer._address;
+  }
+
+  dim(`deployer: ${admin}`);
+
+  cyan(`\nDeploying ATokenYieldSourceProxyFactory...`);
+  const aTokenYieldSourceProxyFactoryResult = await deploy('ATokenYieldSourceProxyFactory', {
+    from: deployer,
+  });
+  displayResult('ATokenYieldSourceProxyFactory', aTokenYieldSourceProxyFactoryResult);
+};
+
+export default deployFunction;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,9 @@ import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import 'hardhat-abi-exporter';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+import 'hardhat-gas-reporter';
 
 import { HardhatUserConfig } from 'hardhat/config';
 
@@ -10,9 +13,32 @@ import networks from './hardhat.network';
 const optimizerEnabled = !process.env.OPTIMIZER_DISABLED;
 
 const config: HardhatUserConfig = {
+  abiExporter: {
+    path: './abis',
+    clear: true,
+    flat: true,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    enabled: process.env.REPORT_GAS ? true : false,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    maxMethodDiff: 10,
+  },
+  mocha: {
+    timeout: 30000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
   networks,
   solidity: {
-    version: '0.7.6',
+    version: '0.6.12',
     settings: {
       optimizer: {
         enabled: optimizerEnabled,
@@ -20,17 +46,6 @@ const config: HardhatUserConfig = {
       },
       evmVersion: 'berlin',
     },
-  },
-  mocha: {
-    timeout: 30000,
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-  abiExporter: {
-    path: './abis',
-    clear: true,
-    flat: true,
   },
   typechain: {
     outDir: 'types',
