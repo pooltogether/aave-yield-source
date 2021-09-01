e18967c1d660ef4a9d772f20e007cd0f27a07aa3
Pierrick Turelier <pierrick@turelier.com>
2021-08-31 21:42:47 -0500

    fix(contracts): fix solidity version to 0.6.12

--- contracts/interfaces/IProtocolDataProvider.sol
+++ contracts/interfaces/IProtocolDataProvider.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 import {ILendingPoolAddressesProvider} from "@aave/protocol-v2/contracts/interfaces/ILendingPoolAddressesProvider.sol";
